524b3dc239233b59d34a60237f5c50b48def3d8c
OlliJuutilainen <ollijuutilainen@mailbox.org>
2025-10-09 01:43:03 +0300

    Add missing Box import in Ouroboros screen

--- android/ouroboros/src/main/java/fi/ouroboros/android/MainActivity.kt
+++ android/ouroboros/src/main/java/fi/ouroboros/android/MainActivity.kt
@@ -12,6 +12,7 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.gestures.awaitEachGesture
 import androidx.compose.foundation.gestures.awaitFirstDown
 import androidx.compose.foundation.gestures.waitForUpOrCancellation
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
